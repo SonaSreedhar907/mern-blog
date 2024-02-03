88075405545eea88207ee69027aa6993adfe1afe
Sona Sreedhar <sonasreedhar44@gmail.com>
2024-02-03 16:28:44 +0530

    sigup added

--- api/index.js
+++ api/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import authRoutes from './routes/auth.routes.js'
 
 dotenv.config();
 
 const app = express();
+app.use(express.json())
 
 const connect = async () => {
     try {
@@ -15,7 +17,20 @@ const connect = async () => {
     }
 }
 
+app.use('/api/auth',authRoutes)
+
 app.listen(5005, () => {
     connect();
     console.log('Server is running on port 5005');
 });
+
+
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500
+    const message = err.message || 'Internal Server Error'
+    res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message
+    })
+})
