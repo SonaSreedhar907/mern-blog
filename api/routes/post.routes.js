6783ce455431f3933a6d7a1b21ec12b5de829b7b
Sona Sreedhar <sonasreedhar44@gmail.com>
2024-04-22 15:57:20 +0530

    delete a post

--- api/routes/post.routes.js
+++ api/routes/post.routes.js
@@ -1,10 +1,13 @@
 import express from 'express'
-import { create } from '../controllers/post.controllers.js'
+import { create,getposts,deletepost } from '../controllers/post.controllers.js'
 import { verifyToken } from '../utils/verifyUser.js'
 
 const router = express.Router()
 
 router.post('/create',verifyToken,create)
 
+router.get('/getposts',getposts)
+
+router.delete('/deletepost/:postId/:userId',verifyToken,deletepost)
 
 export default router
