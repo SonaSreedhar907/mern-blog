012ab97854ca294b2b569d5615bc0c6a681550cf
Sona Sreedhar <sonasreedhar44@gmail.com>
2024-05-04 22:48:59 +0530

    show the comments of post

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,57 +1,59 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { Textarea,Button, Alert } from "flowbite-react";
+import { Textarea, Button, Alert } from "flowbite-react";
+import Comment from "./Comment";
 
 function CommentSection({ postId }) {
   const { currentUser } = useSelector((state) => state.user);
-  const [comment,setComment] = useState('')
-  const [comments,setComments] = useState([])
-  const [commentError,setCommentError] = useState(null)
-  const handleSubmit = async(e)=>{
-        e.preventDefault()
-        if(comment.length>200){
-            return;
-        }
-        try {
-            const res = await fetch('/api/comment/create',{
-                method : 'POST',
-                headers : {
-                    'Content-Type' : 'application/json'
-                },
-                body : JSON.stringify({
-                    content : comment,
-                    postId,
-                    userId : currentUser._id
-                })
-            })
-            const data = await res.json()
-            // console.log('data is ',data)
-            if(res.ok){
-                setComment('')
-                setCommentError(null)
-            }
-        } catch (error) {
-            setCommentError(error.message)
-        }
-  }
-
-  useEffect(()=>{
-    const getComments = async()=>{
-        try {
-            const res = await fetch(`/api/comment/getPostComments/${postId}`)
-            if(res.ok){
-                const data = await res.json()
-                setComments(data)
-            }
-        } catch (error) {
-            console.log(error.message)
-        }
+  const [comment, setComment] = useState("");
+  const [comments, setComments] = useState([]);
+  const [commentError, setCommentError] = useState(null);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (comment.length > 200) {
+      return;
     }
-    getComments()
-  },[postId])
+    try {
+      const res = await fetch("/api/comment/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          content: comment,
+          postId,
+          userId: currentUser._id,
+        }),
+      });
+      const data = await res.json();
+      // console.log('data is ',data)
+      if (res.ok) {
+        setComment("");
+        setCommentError(null);
+        setComments([data,...comments])
+      }
+    } catch (error) {
+      setCommentError(error.message);
+    }
+  };
 
-  console.log('comments are ',comments)
+  useEffect(() => {
+    const getComments = async () => {
+      try {
+        const res = await fetch(`/api/comment/getPostComments/${postId}`);
+        if (res.ok) {
+          const data = await res.json();
+          setComments(data);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    getComments();
+  }, [postId]);
+
+  console.log("comments are ", comments);
   return (
     <div className="max-w-2xl mx-auto w-full p-3">
       {currentUser ? (
@@ -78,26 +80,53 @@ function CommentSection({ postId }) {
         </div>
       )}
       {currentUser && (
-        <form onSubmit = {handleSubmit} className="border border-teal-500 rounded-md p-3">
-            <Textarea placeholder='Add a comment...'
-            rows='3'
-            maxLength='200'
-            onChange = {(e)=>setComment(e.target.value)}
-            value = {comment}
-            />
-            <div className="flex justify-between items-center mt-5">
-                <p className="text-gray-500">{200-comment.length}</p>
-                <Button outline gradientDuoTone='purpleToBlue'
-                type='submit'
-                >
-                 Submit
-                </Button>
-            </div>
-            {commentError && (
-                             <Alert color="failure" className='mt-5'>{commentError}</Alert>
-            )}
+        <form
+          onSubmit={handleSubmit}
+          className="border border-teal-500 rounded-md p-3"
+        >
+          <Textarea
+            placeholder="Add a comment..."
+            rows="3"
+            maxLength="200"
+            onChange={(e) => setComment(e.target.value)}
+            value={comment}
+          />
+          <div className="flex justify-between items-center mt-5">
+            <p className="text-gray-500">{200 - comment.length}</p>
+            <Button outline gradientDuoTone="purpleToBlue" type="submit">
+              Submit
+            </Button>
+          </div>
+          {commentError && (
+            <Alert color="failure" className="mt-5">
+              {commentError}
+            </Alert>
+          )}
         </form>
       )}
+      
+{comments.length === 0 ? (
+    <p className="text-sm my-5">No comments yet!</p>
+):(
+    <>
+          <div className="text-sm my-5 flex items-center gap-1">
+        <p>Comments</p>
+        <div className="border border-gray-400 py-1 px-2 rounded-sm">
+            <p>{comments.length}</p>
+        </div>    
+    </div>
+    {
+      comments.map(comment=>(
+       <Comment
+         key={comment._id}
+         comment={comment}
+       />
+      ))
+    }
+    </>
+)}
+    
+
     </div>
   );
 }
